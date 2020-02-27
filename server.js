c4d6bebf1f5da45393697bb9e2baba0afc7d9988
anirudh <anirudh.chauhan.142@gmail.com>
2020-02-27 19:52:23 +0530

    Server js working

---
+++ server.js
@@ -0,0 +1,33 @@
+const {createServer} = require("http");
+const express = require("express");
+const compression = require("compression");
+const morgan = require("morgan");
+const path = require("path");
+
+const app = express();
+const normalizePort = port => parseInt(port, 10);
+const PORT = normalizePort(process.env.PORT || 5000);
+
+const dev = app.get('env') !== 'production';
+
+if (!dev) {
+	app.disable('x-powered-by');
+	app.use(compression());
+	app.use(morgan('common'));
+	app.use(express.static(path.resolve(__dirname, 'build')));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "build", 'index.html'));
+	});
+}
+if (dev) {
+	app.use(morgan('dev'));
+}
+
+const server = createServer(app);
+
+server.listen(PORT, error => {
+	if (error) throw error;
+	console.log("Server started");
+});
+
+
