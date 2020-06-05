19c626228bd83429db8152db5745b91476775ac8
anirudh <anirudh.chauhan.142@gmail.com>
2020-06-05 23:27:17 +0530

    added multiple activities

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ function App() {
                                 <Route path="about" element={<ResumeContextProvider data={data}/>}/>
                             </Route>
                         </Routes>
-
                     </React.Suspense>
             </Router>
         </GlobalStyleProvider>)
