5a04c484bbbe3ac9cbef70d2ad1da839c7b17e53
anirudh <anirudh.chauhan.142@gmail.com>
2020-06-06 00:54:19 +0530

    added social media links

--- src/ui/Footer.js
+++ src/ui/Footer.js
@@ -2,8 +2,9 @@ import styled from "styled-components";
 
 const Footer = styled.footer`
   padding: 2rem 4rem;
+  margin-top:3rem;
   min-height: 25vh;
-  background: linear-gradient(90deg, ${props => props.theme.palette.primary.main}, ${props => props.theme.palette.primary.light});
+  background-image:  linear-gradient(90deg, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),  linear-gradient(90deg, ${props => props.theme.palette.primary.main}, ${props => props.theme.palette.primary.light});
  
 `
 export default Footer;
