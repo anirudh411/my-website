d78bb174727f8e6e7925d9b83ae60f3b072234b9
anirudh <anirudh.chauhan.142@gmail.com>
2020-12-31 21:13:16 +0530

    added introduction section

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,12 +14,12 @@ export default ({ pageTitle = "", children }) => {
 				<meta charSet="utf-8" />
 				<meta name="description" content={description} />
 				<title>{pageTitle || title}</title>
-				<body className="dark:bg-gray-800 dark:text-gray-100 bg-gray-100" />
+				<body className="dark:bg-gray-900 dark:text-gray-100 bg-gray-100" />
 			</Helmet>
 			<div className="h-screen flex flex-col">
 				<nav className=" max-w-7xl w-full  mx-auto flex flex-row md:flex-col">
 					<AnimateSharedLayout>
-						<ul key={"nav-list"} className="flex justify-end w-full">
+						<ul key={"nav-list"} className="flex justify-end w-full pt-4">
 							{[
 								{ title: "Home", href: "/" },
 								{ title: "About", href: "/about" },
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,70 @@
 import React from "react";
+import { motion } from "framer-motion";
+import HTML5 from "../images/html-5.svg";
+
+const Introduction = () => (
+	<div className="flex mt-6 flex-col items-center">
+		<caption>Hi , I am</caption>
+		<motion.h1
+			className="text-5xl dark:text-purple-500 text-purple-700"
+			initial={{
+				x: 10,
+			}}
+			animate={{
+				x: 0,
+			}}
+		>
+			Anirudh
+		</motion.h1>
+		<motion.h3
+			initial={{
+				x: -10,
+			}}
+			animate={{ x: 0 }}
+			className="text-3xl my-4 dark:text-green-500 text-green-700 "
+		>
+			I develop things for the web.
+		</motion.h3>
+		<motion.p
+			initial={{
+				opacity: 0,
+				rotateX: -45,
+				y: 0,
+			}}
+			animate={{
+				transition: {
+					delay: 0.5,
+				},
+				opacity: 1,
+				rotateX: 0,
+			}}
+			className="mt-6 text-2xl"
+		>
+			Tools and technologies that I have worked with so far
+		</motion.p>
+	</div>
+);
+
+const ToolsContainer = () => {
+	const toolsContainerRef = React.useRef();
+	return (
+		<motion.div ref={toolsContainerRef} className="flex flex-row">
+			<motion.img
+				drag
+				dragConstraints={toolsContainerRef}
+				className="max-h-20"
+				src={HTML5}
+				alt={"HTML 5"}
+			/>
+		</motion.div>
+	);
+};
 
 export default () => {
-	return <div>Index</div>;
+	return (
+		<>
+			<Introduction />
+			{/*<ToolsContainer />*/}
+		</>
+	);
 };
